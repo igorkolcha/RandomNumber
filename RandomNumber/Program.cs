573384a8ceedf10eb4f2a04e69cf4948c20ea8e0
user <user@WIN-6VKSD92BLCI>
2023-06-22 09:14:48 +0300

    Добавьте файлы проекта.

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            var rand=new Random();
+            Console.WriteLine("Random number {0}",rand.Next(0,100));
+            Console.Read();
+        }
+    }
+}
